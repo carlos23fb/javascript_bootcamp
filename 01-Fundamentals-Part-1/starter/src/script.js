fcbc6a7e30c637e0365a31623e23f27475912f6a
Carlos <jcflobe@gmail.com>
2020-12-15 14:45:48 -0700

    math operators cap

---
+++ 01-Fundamentals-Part-1/starter/src/script.js
@@ -0,0 +1,31 @@
+// let js = 'amazing';
+// if (js === 'amazing') {
+//     console.log('Amazing!');
+// }
+
+// let firstName = 'Juan';
+// console.log(firstName);
+
+// TODO 
+// ! Warning!
+// * Note 
+
+
+// isJavascriptFun = false;
+// console.log(isJavascriptFun);
+// console.log(typeof(isJavascriptFun));
+
+
+
+// * Operators
+const YEAR = 2037; 
+const ageJonas = YEAR - 1991;
+const ageSarah = YEAR - 2018;
+console.log(ageJonas);
+console.log(ageSarah);
+
+console.log(ageJonas - ageSarah);
+
+const firstName = "Carlos";
+const lastName = 'Becerra';
+console.log(firstName + ' ' + lastName);
