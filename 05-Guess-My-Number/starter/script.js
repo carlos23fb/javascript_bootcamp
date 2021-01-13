6c23cd77f789867447f14f1a83182fd4219b3e2b
Carlos <jcflobe@gmail.com>
2021-01-13 11:29:11 -0700

    handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.querySelector('.check').onclick = () => {
+//         document.querySelector('.message').textContent = 'Correct Number';
+//         console.log(document.querySelector('.guess').value);
+//     };
+// })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.check').addEventListener('click', () => {
+        document.querySelector('.message').textContent = 'Correct!';
+    })
+})
