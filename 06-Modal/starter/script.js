d9e6208e946a3f959b79ac73bcbc2a7b6d947366
Carlos <jcflobe@gmail.com>
2021-01-14 17:26:12 -0700

    add event listener for all show modal buttons

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+    const btnCloseModal = document.querySelector('.close-modal');
+    const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+    function close(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+
+    function open(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+
+    }
+
+    
+    btnsOpenModal.forEach(element => {
+        element.addEventListener('click', open)
+    });
+
+    btnCloseModal.addEventListener('click', close);
+    overlay.addEventListener('click', close);
+
+
+    document.addEventListener('keydown', function(e){
+        if (e.key === 'Escape' && !modal.classList.contains('hidden')){
+            close()
+        }
+    })
+})
