f705d01c64eb0e088b76b16681d3504812807e72
carlos23fb <jcflobe@gmail.com>
2022-01-25 04:50:14 -0700

    object destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -9,12 +9,12 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  
-  order : function(starterIndex, mainIndex){
+
+  order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  weekdays : ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-  openingHours : {
+  weekdays: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+  openingHours: {
     thus: {
       open: 12,
       close: 22,
@@ -27,31 +27,48 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     }
+  },
+  orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    console.log(`Address: ${address}, time: ${time}, main: ${this.mainMenu[mainIndex]} starter: ${this.starterMenu[starterIndex]}`);
   }
 };
 
 
+restaurant.orderDelivery({
+  address: "Mariano Escobedo #112"
+})
+
+
 // * Destructuring Objects
 
-let {name:name_res, openingHours, categories} = restaurant;
+let { name: name_res, openingHours, categories } = restaurant;
 
 // console.log(name_res, openingHours, categories);
 
 // * Default Values
-const {menu = [], starterMenu : stater = [] } = restaurant;
+const { menu = [], starterMenu: stater = [] } = restaurant;
 
 
 // * Mutating variables
 let a = 111;
 let b = 999;
-const obj = {a: 23, b: 7, c:14};
-({a, b} = obj);
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
 
 // * Nested Objects
 
 // let {thus, fri, sat} = restaurant.openingHours
 
-let {fri: {open: o, close: c}} = restaurant.openingHours
+let { fri: { open: o, close: c } } = restaurant.openingHours
+
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2
+})
+
 
 
 
