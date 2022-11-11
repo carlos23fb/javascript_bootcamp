50e4f3ca091b1157b174083c88804095398ba187
carlos23fb <jcflobe@gmail.com>
2022-11-10 19:48:52 -0700

    Converting and Checking Numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -251,3 +251,26 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// TODO Cheking and creating numbers
+
+console.log(23 === 23.0)
+
+console.log(0.1 + 0.2)
+
+// Conversion
+console.log(Number('23'))
+console.log(+'23')
+
+// Parsing
+
+console.log(Number.parseInt('30px'))
+
+console.log(Number.parseFloat('2.5.rem'))
+
+console.log(Number.isNaN('20'))
+console.log(Number.isNaN(20))
+
+// Checking if value is a numbers
+console.log(Number.isFinite(20))
+console.log(Number.isFinite('20'))
