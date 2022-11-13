6d8381954d8f6dfaa0a370d80beb696c2696e9a1
carlos23fb <jcflobe@gmail.com>
2022-11-12 19:00:56 -0700

    Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -254,23 +254,57 @@ btnSort.addEventListener('click', function (e) {
 
 // TODO Cheking and creating numbers
 
-console.log(23 === 23.0)
+// console.log(23 === 23.0)
 
-console.log(0.1 + 0.2)
+// console.log(0.1 + 0.2)
 
 // Conversion
-console.log(Number('23'))
-console.log(+'23')
+// console.log(Number('23'))
+// console.log(+'23')
 
 // Parsing
 
-console.log(Number.parseInt('30px'))
+// console.log(Number.parseInt('30px'))
 
-console.log(Number.parseFloat('2.5.rem'))
+// console.log(Number.parseFloat('2.5.rem'))
 
-console.log(Number.isNaN('20'))
-console.log(Number.isNaN(20))
+// console.log(Number.isNaN('20'))
+// console.log(Number.isNaN(20))
 
 // Checking if value is a numbers
-console.log(Number.isFinite(20))
-console.log(Number.isFinite('20'))
+// console.log(Number.isFinite(20))
+// console.log(Number.isFinite('20'))
+
+
+// console.log(Math.sqrt(5))
+
+// console.log(Math.max(5,18))
+
+
+// console.log(Math.min(1, 2))
+
+
+// console.log(Math.PI)
+
+
+const randomInt = (min, max) => Math.flor(Math.random() * ((max - min) + 1) + min)
+
+// console.log(randomInt(1, 3))
+
+// * Rounding  integers
+
+console.log(Math.trunc(23.3))
+
+console.log(Math.round(23.4))
+
+
+console.log('--------')
+
+
+console.log(Math.ceil(23.3))
+console.log(Math.ceil(23.9))
+
+
+// Rounding decimals
+
+console.log((2.7).toFixed(0))
