69c7201a79e5dfa7869624299056c9e8e5e09a20
carlos23fb <jcflobe@gmail.com>
2022-11-13 12:29:05 -0700

    Adding Dates to "Bankist" App

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -81,18 +81,26 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i])
+    const day = `${date.getDate()}`.padStart(2, 0)
+    const month = `${date.getMonth() + 1}`.padStart(2, 0)
+    const year = date.getFullYear()
+    const displayDate = `${day}/${month}/${year}`
+    
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1
       } ${type}</div>
+      <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
@@ -141,7 +149,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -154,9 +162,22 @@ const updateUI = function (acc) {
 // Event handlers
 let currentAccount;
 
+
+currentAccount = account1
+updateUI(currentAccount)
+containerApp.style.opacity = 100;
+
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
+
+  const now = new Date();
+  const day = now.getDate()
+  const month = now.getMonth() + 1
+  const year = now.getFullYear()
+  const hour = now.getHours()
+  const minute = now.getMinutes()
+  labelDate.textContent = `${String(day).padStart(2, '0')}/${String(month).padStart(2, '0')}/${year}, ${hour}:${minute}`
 
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
@@ -194,7 +215,9 @@ btnTransfer.addEventListener('click', function (e) {
   ) {
     // Doing the transfer
     currentAccount.movements.push(-amount);
+    currentAccount.movementsDates.push(new Date().toISOString())
     receiverAcc.movements.push(amount);
+    receiverAcc.movementsDates.push(new Date().toISOString())
 
     // Update UI
     updateUI(currentAccount);
@@ -209,6 +232,7 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -242,7 +266,7 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
@@ -339,7 +363,7 @@ document.querySelector('.logo').addEventListener('click', e => {
   const allmovements = [...document.querySelectorAll(".movements__row")]
 
   allmovements.forEach((row, i) => {
-    if(i % 2 === 0) row.style.backgroundColor = 'orangered'
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered'
   })
 })
 
@@ -362,7 +386,7 @@ document.querySelector('.logo').addEventListener('click', e => {
 // * FIXME: Create a date
 
 
-const now = new Date()
+// const now = new Date()
 // console.log(now)
 
 // console.log(new Date('Nov 12 2022 19:43:04'))
@@ -384,7 +408,7 @@ const now = new Date()
 const future = new Date(2037, 10, 19, 15, 23)
 console.log(future)
 console.log(future.getFullYear())
-console.log(future.getMonth()+1)
+console.log(future.getMonth() + 1)
 console.log(future.getTime())
 
 console.log(new Date(2142282180000))
