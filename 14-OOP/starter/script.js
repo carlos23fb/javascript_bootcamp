b08b612718679b90d9e917e135eb144cb1eb6d14
carlos23fb <jcflobe@gmail.com>
2022-11-21 17:38:15 -0700

    Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+
+// TODO: Contstructor functions and the "new" opetator
+
+const Person =  function(firstName, birthYear){
+
+    // Instance properties
+    this.firstName = firstName,
+    this.birthYear = birthYear
+
+    this.calcAge = function(){
+        console.log(2022 - this.birthYear)
+    }
+}
+
+const jonas = new Person('Jonas', 1991)
+
+
+jonas.nickname = 'javascript redentor'
+
+console.log(jonas)
+
+const matilda = new Person('Matilda', 1994)
+
+matilda.nickname = 'psyco'
+
+console.log(matilda)
+
+
+console.log(jonas instanceof Person)
+
+console.log(matilda.calcAge())
