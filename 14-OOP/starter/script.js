90c62d884040b8c1d18551579718d20c82738c75
carlos23fb <jcflobe@gmail.com>
2022-11-21 18:00:14 -0700

    Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -9,9 +9,6 @@ const Person =  function(firstName, birthYear){
     this.firstName = firstName,
     this.birthYear = birthYear
 
-    this.calcAge = function(){
-        console.log(2022 - this.birthYear)
-    }
 }
 
 const jonas = new Person('Jonas', 1991)
@@ -19,15 +16,41 @@ const jonas = new Person('Jonas', 1991)
 
 jonas.nickname = 'javascript redentor'
 
-console.log(jonas)
+// console.log(jonas)
 
 const matilda = new Person('Matilda', 1994)
 
 matilda.nickname = 'psyco'
 
+// console.log(matilda)
+
+// console.log(jonas instanceof Person)
+
+// console.log(matilda.calcAge())
+
+// TODO: Prototypes
+
+
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function () {
+    console.log(2022 - this.birthYear)
+}
+
+// console.log(matilda.calcAge())
+
+jonas.calcAge()
+matilda.calcAge()
+
+console.log(jonas.__proto__ === Person.prototype)
+
+console.log(Person.prototype.isPrototypeOf(jonas))
+console.log(Person.prototype.isPrototypeOf(Person))
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(jonas)
 console.log(matilda)
 
+console.log(jonas.hasOwnProperty('firstName'))
 
-console.log(jonas instanceof Person)
-
-console.log(matilda.calcAge())
+console.log(jonas.hasOwnProperty('species'))
