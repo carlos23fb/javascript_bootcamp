6149007b6d4e0597efb77a4c6c460384b2b236b7
carlos23fb <jcflobe@gmail.com>
2022-11-26 20:46:13 -0700

    Displaying a Map Using Leaflet Library

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,9 +13,23 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation.getCurrentPosition(
     function (position) {
-        const {latitude} = position.coords
-        const {longitude} = position.coords
+        const { latitude } = position.coords
+        const { longitude } = position.coords
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+
+        const coords = [latitude, longitude]
+
+        const map = L.map('map')
+        map.setView(coords, 17);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
 
     },
     function () {
