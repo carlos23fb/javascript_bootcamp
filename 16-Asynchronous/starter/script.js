d90aecf1138e21a1cde8b011f6a5ecd636aba08e
carlos23fb <jcflobe@gmail.com>
2022-12-05 18:48:04 -0700

    Welcome to Callback Hell & Promises and the Fetch API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,9 +7,67 @@ const countriesContainer = document.querySelector('.countries');
 // https://restcountries.com/v2/
 
 ///////////////////////////////////////
-const getCountryData = function (name) {
+
+
+// const getCountryData = function (name) {
+//     console.log(name)
+//     let data;
+
+//     const request = new XMLHttpRequest();
+
+//     request.open('GET', `https://restcountries.com/v3.1/name/${name}`)
+
+//     request.send();
+
+//     request.addEventListener('load', function () {
+
+//         [data] = JSON.parse(this.responseText)
+
+//         console.log(data)
+
+//         const language = Object.entries(data.languages)
+//         const currency = Object.entries(data.currencies)
+
+
+
+//         let html = `
+//     <article class="country">
+//           <img class="country__img" src="${data.flags.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${+data.population/1000000} people</p>
+
+//           </div>
+//     </article>`
+
+//         countriesContainer.insertAdjacentHTML('afterend', html)
+
+//     })
+// }
+
+
+const renderCountry = function (data, className = '') {
+    let html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flags.png}"/>
+            <div class="country__data">
+                <h3 class="country__name">${data.name.common}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${+data.population / 1000000} people</p>
+            </div>
+        </article>`
+
+    countriesContainer.insertAdjacentHTML('beforeend', html)
+    countriesContainer.style.opacity = 1;
+}
+
+
+const getCountryAndNeighbour = function (name) {
     console.log(name)
     let data;
+
+    // * AJAX call country one
 
     const request = new XMLHttpRequest();
 
@@ -23,27 +81,42 @@ const getCountryData = function (name) {
 
         console.log(data)
 
-        const language = Object.entries(data.languages)
-        const currency = Object.entries(data.currencies)
+        // * Render country
 
-       
+        renderCountry(data)
 
-        let html = `
-    <article class="country">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${data.population} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages.spa}</p>
-            <p class="country__row"><span>💰</span>${data.currencies.MXN.name}</p>
-          </div>
-    </article>`
+        // * Render neighbours
 
-        countriesContainer.insertAdjacentHTML('afterend', html)
+        const neighbour = data.borders?.[0]
+
+        if (!neighbour) return
+
+
+
+        const request2 = new XMLHttpRequest();
+
+        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`)
+
+        request2.send();
+
+
+        request2.addEventListener('load', function () {
+
+            [data] = JSON.parse(this.responseText)
+
+            console.log(data)
+
+            // * Render country
+
+            renderCountry(data, 'neighbour')
+
+        })
 
     })
 }
 
-getCountryData('mexico')
-getCountryData('')
+
+const request = fetch('https://restcountries.com/v3.1/name/mexico')
+
+
+console.log(request)
