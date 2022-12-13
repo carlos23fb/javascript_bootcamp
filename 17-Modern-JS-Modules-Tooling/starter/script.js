fbebde76af01aec735c65de3b03c7b8ef4247515
carlos23fb <jcflobe@gmail.com>
2022-12-12 19:09:46 -0700

    Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -4,12 +4,12 @@ import { addToCart, totalPrice as price, totalQuantity, cart } from './shoopingC
 
 import * as ShoopingCart from './shoopingCart.js'
 
-console.log('Importing module')
+// console.log('Importing module')
 
 addToCart('Llave nuda 5"', 13)
 
 
-console.log(price, totalQuantity)
+// console.log(price, totalQuantity)
 
 
 ShoopingCart.addToCart('Lonche de carne asada', 2)
@@ -21,4 +21,36 @@ import add from './shoopingCart.js'
 
 add('Queso', 6)
 
-console.log(cart)
+// console.log(cart)
+
+// TODO Top level await
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+// const data = await res.json();
+
+// console.log(data)
+
+// console.log('Last line of code')
+
+const getLastPost = async function () {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    const data = await res.json();
+
+    return { title: data.at(-1).title, text: data.at(-1).body }
+}
+
+const lastPost = getLastPost();
+console.log(lastPost)
+
+
+// Not very clean
+
+// lastPost.then(res => {
+//     console.log(res)
+// })
+
+const lastPost2 = await getLastPost()
+console.log(lastPost2)
